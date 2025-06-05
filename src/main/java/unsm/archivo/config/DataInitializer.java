e99b63bf742834d8e43d81e4d3c5a1e4b42d46da
DraftraX <wmanuelga@gmail.com>
2025-06-05 01:43:18 -0500

    datainitialaizer

--- src/main/java/unsm/archivo/config/DataInitializer.java
+++ src/main/java/unsm/archivo/config/DataInitializer.java
@@ -1,57 +1,57 @@
-package unsm.archivo.config;
+// package unsm.archivo.config;
 
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.Set;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-import unsm.archivo.entitys.Cargo;
-import unsm.archivo.entitys.Usuario;
-import unsm.archivo.repository.CargoRepo;
-import unsm.archivo.repository.UsuarioRepo;
+// import unsm.archivo.entitys.Cargo;
+// import unsm.archivo.entitys.Usuario;
+// import unsm.archivo.repository.CargoRepo;
+// import unsm.archivo.repository.UsuarioRepo;
 
-@Configuration
-public class DataInitializer {
+// @Configuration
+// public class DataInitializer {
 
-    @Bean
-    CommandLineRunner initializeData(UsuarioRepo usuarioRepository,
-            PasswordEncoder passwordEncoder,
-            CargoRepo rolRepository) {
-        return args -> {
-            Cargo administrador = new Cargo();
-            administrador.setName("ADMINISTRADOR");
-            rolRepository.save(administrador);
-            Cargo jefeArchivo = new Cargo();
-            jefeArchivo.setName("JEFE ARCHIVO");
-            rolRepository.save(jefeArchivo);
-            Cargo secretaria = new Cargo();
-            secretaria.setName("SECRETARIA");
-            rolRepository.save(secretaria);
-            Cargo usuarioCargo = new Cargo();
-            usuarioCargo.setName("USUARIO");
-            rolRepository.save(usuarioCargo);
+// @Bean
+// CommandLineRunner initializeData(UsuarioRepo usuarioRepository,
+// PasswordEncoder passwordEncoder,
+// CargoRepo rolRepository) {
+// return args -> {
+// Cargo administrador = new Cargo();
+// administrador.setName("ADMINISTRADOR");
+// rolRepository.save(administrador);
+// Cargo jefeArchivo = new Cargo();
+// jefeArchivo.setName("JEFE ARCHIVO");
+// rolRepository.save(jefeArchivo);
+// Cargo secretaria = new Cargo();
+// secretaria.setName("SECRETARIA");
+// rolRepository.save(secretaria);
+// Cargo usuarioCargo = new Cargo();
+// usuarioCargo.setName("USUARIO");
+// rolRepository.save(usuarioCargo);
 
-            Usuario usuario = new Usuario();
-            Set<Cargo> cargos = new HashSet<>();
-            Cargo cargo = rolRepository.findByName("ADMINISTRADOR")
-                    .orElseThrow(() -> new RuntimeException("Cargo no encontrado"));
-            cargos.add(cargo);
-            usuario.setUsername("eduysting@gmail.com");
-            usuario.setEstado("Activo");
-            usuario.setDni("12345678");
-            usuario.setName("Eduardo Abel");
-            usuario.setLastname("Padilla Coral");
-            usuario.setAddress(" Jirón José Carlos Mariategui n° 123");
-            usuario.setPhone("123456789");
-            usuario.setPassword(passwordEncoder.encode("123456"));
-            usuario.setCargos(cargos);
-            usuarioRepository.save(usuario);
-        };
-    }
-}
+// Usuario usuario = new Usuario();
+// Set<Cargo> cargos = new HashSet<>();
+// Cargo cargo = rolRepository.findByName("ADMINISTRADOR")
+// .orElseThrow(() -> new RuntimeException("Cargo no encontrado"));
+// cargos.add(cargo);
+// usuario.setUsername("eduysting@gmail.com");
+// usuario.setEstado("Activo");
+// usuario.setDni("12345678");
+// usuario.setName("Eduardo Abel");
+// usuario.setLastname("Padilla Coral");
+// usuario.setAddress(" Jirón José Carlos Mariategui n° 123");
+// usuario.setPhone("123456789");
+// usuario.setPassword(passwordEncoder.encode("123456"));
+// usuario.setCargos(cargos);
+// usuarioRepository.save(usuario);
+// };
+// }
+// }
 
 // MAS ROLES Y USUARIOS
 // package unsm.archivo.config;
